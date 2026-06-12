20ae2e0d029218d9226d2feb7e4dc9fb556a6d85
yousef <yousefbenrabea@gmail.com>
2026-06-12 17:13:23 +0100

    fix: make trigger creation idempotent by adding DROP TRIGGER IF EXISTS

--- supabase/migrations/20260612000000_init.sql
+++ supabase/migrations/20260612000000_init.sql
@@ -248,6 +248,7 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql SECURITY DEFINER;
 
+DROP TRIGGER IF EXISTS on_auth_user_created ON auth.users;
 CREATE TRIGGER on_auth_user_created
   AFTER INSERT ON auth.users
   FOR EACH ROW EXECUTE FUNCTION public.handle_new_user();
